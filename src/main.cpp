b84645daa15f1c348fa82b6dd889f0a5311761c3
Eric Schneider <u1643364@live.unc.edu>
2022-12-26 17:38:06 -0500

    we'll call this a WIP

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,20 @@
 
 using namespace std;
 
-int main(){
+class State {
+
+};
+
+int main(int argc, char **argv){
+	if (argc != 2) {
+		cerr << "Incorrect amount of arguments (should only accept one: the path)" << endl;
+		return 1;
+	}
+
+	char *path = argv[1];
+
+	FILE *file = fopen(path, "rw");
+
 	cout << "Hello world!" << endl;
 	return 0;
 }
