7cef7cba486417f836cca3777c282c958a6b0c06
Eric Schneider <u1643364@live.unc.edu>
2022-12-27 21:50:50 -0500

    some experimentation

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <sys/stat.h>
 
 using namespace std;
 
@@ -14,7 +16,29 @@ int main(int argc, char **argv){
 
 	char *path = argv[1];
 
-	FILE *file = fopen(path, "rw");
+	struct stat sb;
+
+	FILE *file = nullptr;
+	vector<string> buffer;
+
+	if (stat(path, &sb) == 0) {
+		if (sb.st_mode & S_IFDIR) {
+			cerr << "Cannot edit directory!" << endl;
+			return 1;
+		}
+
+		FILE *file = fopen(path, "r");
+		if (file == nullptr) {
+			cerr << "Failed to open path!" << endl;
+			return 1;
+		}
+
+		// write to buffer
+	} else {
+		//cout << "The Path is invalid!" << endl;
+		//file = fopen(path, "a+");
+		// initiate buffer
+	}
 
 	cout << "Hello world!" << endl;
 	return 0;
